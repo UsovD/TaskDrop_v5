ea9b4f424017280497ab9f4fcdd7ad4d8a5fee39
Usov <usov.denis.a@gmail.com>
2025-04-18 16:46:16 +0400

    Add subtitle 'Управляй своими делами' and fix header styling

--- src/App.tsx
+++ src/App.tsx
@@ -165,7 +165,8 @@ const MainPage: React.FC = () => {
       )}
       
       <div className="app-header">
-        <h1>TaskDrop</h1>
+        <h1 className="m-0 pt-4 px-4">TaskDrop</h1>
+        <p className="text-subtitle px-4 mt-1 mb-0">Управляй своими делами</p>
         <UserInfo />
       </div>
       
